4fd245cd2c13abcf3252b61018a4eed305dbd11a
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-11-08 12:30:02 +0000

    [add]comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :post_images,only: [:new,:create,:show,:index,:destroy]
+  resources :post_images,only: [:new,:create,:show,:index,:destroy] do
+    resources :post_comments,only: [:create, :destroy]
+  end
   resources :users,only: [:show,:edit,:update]
   root to: "homes#top"
   get "homes/about" => "homes#about",as: :about
